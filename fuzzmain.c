920fcbd33ffd371a993536ad0a6155ca357d030b
Nelson Elhage <nelhage@nelhage.com>
2018-04-15 18:52:20 -0700

    init gvc once

--- fuzzmain.c
+++ fuzzmain.c
@@ -5,16 +5,19 @@
 #include <stdio.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    static GVC_t *gvc = NULL;
+    if (gvc == NULL) {
+        gvc = gvContext();
+        agseterr(AGMAX);
+    }
     FILE *fp = fmemopen((void*)Data, Size, "r");
     if (fp == NULL) {
         return 0;
     }
-    GVC_t *gvc = gvContext();
     Agraph_t* G = agread(fp, 0);
     fclose(fp);
     if (G != NULL) {
         agclose(G);
     }
-    gvFreeContext(gvc);
     return 0;
 }
